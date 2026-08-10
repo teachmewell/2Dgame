262ac7cea51796ac039b0571d833114b4069a0d8
teachmewell <coyu3@duck.com>
2026-08-10 17:04:34 +0200

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,18 +3,20 @@ import objects.Tree;
 
 public class Board{
 
-int[][] board; //first is width, then height
+int[][] treeBoard; //first is width, then height
 
+  
 public Board(int width, int height){
-this.board = new int[width][height];
+this.treeBoard = new int[width][height];
 }
 
 public void createEnvironment(){
   for (int height = 0; height < y.length; height++){
 for(int width = 0; width < x.length(); width++){
-if(Math.random()> 0.7){ this.board[width][height] = 1; }
+if(Math.random()> 0.7){ this.treeBoard[width][height] = 1; }
 }  
 }
+
 }
 
 
