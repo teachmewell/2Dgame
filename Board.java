440fe101be6cab29eecebca46c4ea1b0deb2c052
teachmewell <coyu3@duck.com>
2026-08-10 18:35:59 +0200

    Update Board.java

--- Board.java
+++ Board.java
@@ -17,14 +17,14 @@ this.fullBoard = new int[width][height];
 }
 
 public void createEnvironment(){
-  for (int height = 0; height < fullBoard[0].length(); height++){
-for(int width = 0; width < fullBoard.length(); width++){
+  for (int height = 0; height < fullBoard[0].length; height++){
+for(int width = 0; width < fullBoard.length; width++){
 if(Math.random()> 0.7){ this.treeBoard[width][height] = new Tree(); fullBoard[width][height] = 1;}
 }  
 }
   
-for (int height = 0; height < fullBoard[0].length(); height++){
-for(int width = 0; width < fullBoard.length(); width++){
+for (int height = 0; height < fullBoard[0].length; height++){
+for(int width = 0; width < fullBoard.length; width++){
   if(treeBoard[width][height] == null){
 if(Math.random()> 0.7){ this.rockBoard[width][height] = new Rock(); fullBoard[width][height] = 1;}
 }
