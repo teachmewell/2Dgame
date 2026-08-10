8e76b1de6a27c751f71dcf946aa61afdddf04c23
teachmewell <coyu3@duck.com>
2026-08-10 16:59:55 +0200

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,14 +1,21 @@
+import java.lang.Math;
+import objects.Tree;
+
 public class Board{
 
-int[] x;
-int[] y;
+int[][] board; //first is width, then height
 
-public Board(int height, int width){
-this.x = new int[width];
-this.y = new int[height];
+public Board(int width, int height){
+this.board = new int[width][height];
 }
 
-
+public void createEnvironment(){
+  for (int height = 0; height < y.length; height++){
+for(int width = 0; width < x.length(); width++){
+if(Math.random()> 0.7){ this.board[width][height] = 1; }
+}  
+}
+}
 
 
 }
