22641cd858002d256bd58cb5f8f5f139c297045f
teachmewell <coyu3@duck.com>
2026-08-10 16:52:38 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
-    public static void main(String[] args) {
-       
+    
+public static void main(String[] args) {
+Board board = new Board(1000, 1000);
+board.createEnvironment();
+    Player player = new Player();
+
+
+        
     }
 }
