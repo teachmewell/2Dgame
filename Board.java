82a69c2168155c1b6a0dc119ba1d075d704be670
teachmewell <coyu3@duck.com>
2026-08-10 17:09:29 +0200

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,22 +1,32 @@
 import java.lang.Math;
 import objects.Tree;
+import objects.Rock;
 
 public class Board{
 
 int[][] treeBoard; //first is width, then height
+int[][] rockBoard; //first is width, then height
 
   
 public Board(int width, int height){
-this.treeBoard = new int[width][height];
+this.treeBoard = new Tree[width][height];
+  this.rockBoard = new Rock[width][height];
 }
 
 public void createEnvironment(){
   for (int height = 0; height < y.length; height++){
 for(int width = 0; width < x.length(); width++){
-if(Math.random()> 0.7){ this.treeBoard[width][height] = 1; }
+if(Math.random()> 0.7){ this.treeBoard[width][height] = new Tree(); }
 }  
 }
-
+  
+for (int height = 0; height < y.length; height++){
+for(int width = 0; width < x.length(); width++){
+  if(treeBoard[width][height] == null){
+if(Math.random()> 0.7){ this.rockBoard[width][height] = new Rock(); }
+}
+}
+}
 }
 
 
